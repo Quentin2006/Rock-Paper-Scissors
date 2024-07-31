7ee5612007faf465a867825081fbfc406d88ee4b
Quentin Scheetz <124484154+Quentin2006@users.noreply.github.com>
2024-07-31 15:56:21 -0700

    added fucntion for human input and fuction that can chose a winner

--- scripts.js
+++ scripts.js
@@ -1,16 +1,55 @@
+let humanScore = 0;
+let computerScore = 0;
+
 // Returns "rock", "paper" or "scissors" using a random #
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
 
   if (choice === 0) {
-    return "rock"
-  }
-  if (choice === 1) {
-    return "paper"
-  }
-  if (choice === 2) {
-    return "scissors"
+    return "rock";
+  } else if (choice === 1) {
+    return "paper";
+  } else if (choice === 2) {
+    return "scissors";
   }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let choice = prompt("rock(r), paper(p), or scissors(s)");
+    choice = choice.toUpperCase();
+    if (choice === "r" || choice === "rock") {
+      valid = true;
+      return "rock";
+    } else if (choice === "p" || choice === "paper") {
+      valid = true;
+      return "paper";
+    } else if (choice === "s" || choice === "scissors") {
+      valid = true;
+      return "scissors";
+    } else return "not valid"
+}
+
+function playRound(humanChoice, computerChoice) {
+  switch (true) {
+    case computerChoice == "rock" && humanChoice == "paper":
+      humanChoice++;
+      return "You win!";
+    case humanChoice == "rock" && computerScore == "paper":
+      computerScore++;
+      return "You lose!";
+    case computerScore == "rock" && humanChoice == "scissors":
+      computerScore++;
+      return "You lose!";
+    case humanChoice == "rock" && computerScore == "scissors":
+      humanChoice++;
+      return "You win!";
+    case computerScore == "scissors" && humanChoice == "paper":
+      computerScore++;
+      return "You lose!";
+    case humanChoice == "scissors" && computerScore == "paper":
+      computerScore++;
+      return "You win!";
+    default:
+      return "Tie!";
+  }
+}
